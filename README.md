037ac2611256bb62b4611e6c204e1b582d79fe09
1d8 <bnu1s@protonmail.com>
2023-03-14 23:10:47 -0700

    Added GetMinValue() & GetMaxValue() functions

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 Function: `GetSQRoot(number)`
 
 * Pass any integer when calling the function, returns the square root of that integer
+
+Function: `GetMaxValue(integerList)`
+
+* Pass a list of integers or tuple of integers, returns max value from that list/tuple of values
+
+Function: `GetMinValue(integerList)`
+
+* Pass a list of integers or tuple of integers, returns min value from that list/tuple of values
--- functions.py
+++ functions.py
@@ -1,8 +1,24 @@
 import math
 
+# number -> must be integer
 def GetSQRoot(number):
     try:
         return math.sqrt(number)
     except TypeError:
         return "Error: You must input an integer!"
 
+# integerList -> must be list or tuple
+def GetMaxValue(integerList):
+    if isinstance(integerList, list) or isinstance(integerList, tuple):
+        # continue if it's list
+        return max(integerList)
+    else:
+        return "Error: Must pass a list or tuple"
+
+# integerList -> must be list or tuple
+def GetMinValue(integerList):
+    if isinstance(integerList, list) or isinstance(integerList, tuple):
+        # continue if it's list
+        return min(integerList)
+    else:
+        return "Error: Must pass a list or tuple"
